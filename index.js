e63da6912cfe6947e49e57c0172b750d41e507b6
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-23 10:51:56 +0600

    token done

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ const app = express()
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 5009
 const cors = require('cors')
-app.use(cors({
-   origin: [
-     'http://localhost:5173',
-   ],
-   credentials: true
-}))
+app.use(cors())
 app.use(express.json())
-app.use(cookieParser())
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f8w8siu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -28,28 +22,7 @@ const client = new MongoClient(uri, {
     }
   });
 
-  const verifyToken = async(req,res,next)=>{
-       const token = req.cookies?.token 
-      //  console.log('value of verify token',token);
-       if(!token){
-         return res.status(401).send({message: 'not autorized'})
-       }
-       jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-        if(err){
-          console.log(err);
-          return res.status(401).send({message: 'unauthorized'})
-        }
-        req.user = decoded
-        next()
-       })
-       
-  }
-
-  const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-  };
+  
 
   async function run() {
     try { 
@@ -66,7 +39,20 @@ const client = new MongoClient(uri, {
     //    .cookie('token',token,cookieOptions)
     //    .send({success: true}) 
     // })
-
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
@@ -79,7 +65,8 @@ const client = new MongoClient(uri, {
 
     })
 
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyToken, async(req,res)=>{
+        
         const result = await userCollection.find().toArray()
         res.send(result)
     })
